fe49998363ecbfae9535731e41dafce6c6f8d596
Alex-prog814 <aleks.98.x.28@gmail.com>
2023-02-08 12:23:33 +0600

    Added CRU-logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import AddTodo from './components/AddTodo/AddTodo';
+import TodoList from './components/TodoList/TodoList';
+import EditTodo from './components/EditTodo/EditTodo';
 
-function App() {
+const App = () => {
+  let [todos, setTodos] = useState([]);
+  let [editedObj, setEditedObj] = useState(null);
+
+  function createTodo(todoObj) {
+    let newTodos = [...todos];
+    newTodos.push(todoObj);
+    // console.log(newTodos);
+    setTodos(newTodos);
+  };
+
+  function changeStatus(id) {
+    const newTodos = todos.map(item => {
+      if(item.id === id) {
+        item.status = !item.status;
+      };
+      return item;
+    });
+    setTodos(newTodos);
+  };
+
+  function getEditedObj(id) {
+    let oneObj = todos.find(item => item.id === id);
+    setEditedObj(oneObj);
+    // console.log(oneObj);
+  };
+
+  function saveEditedObj(newObj) {
+    let newTodos = todos.map(item => {
+      if(item.id === newObj.id) {
+        return newObj;
+      } else {
+        return item;
+      };
+    });
+    setTodos(newTodos);
+    setEditedObj(null);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <AddTodo createTodo={createTodo} />
+      <TodoList todos={todos} changeStatus={changeStatus} getEditedObj={getEditedObj} />
+      {editedObj ? (
+        <EditTodo editedObj={editedObj} saveEditedObj={saveEditedObj} />
+      ) : (
+        <h4>Not editing todo now!</h4>
+      )}
+    </>
+  )
+};
 
 export default App;
